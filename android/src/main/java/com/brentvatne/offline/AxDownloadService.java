96b4dcdb658030d30a93776758eec859fbe77a34
Daniel Marín <daniel.marin@start.cat>
2024-07-04 13:05:42 +0200

    offline

---
+++ android/src/main/java/com/brentvatne/offline/AxDownloadService.java
@@ -0,0 +1,140 @@
+package com.brentvatne.offline;
+
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.brentvatne.R;
+import androidx.media3.exoplayer.offline.Download;
+import androidx.media3.exoplayer.offline.DownloadManager;
+import androidx.media3.exoplayer.offline.DownloadService;
+import androidx.media3.exoplayer.scheduler.PlatformScheduler;
+import androidx.media3.exoplayer.offline.DownloadNotificationHelper;
+import androidx.media3.common.util.NotificationUtil;
+import androidx.media3.common.util.Util;
+
+import java.util.List;
+
+/**
+ * A class that extends Exoplayer's DownloadService class.
+ * Defines a service that enables the downloads to continue even when the
+ * app is in background.
+ */
+public class AxDownloadService extends DownloadService {
+
+    private static final String CHANNEL_ID = "download_channel";
+    private static final int JOB_ID = 1;
+    private static final int FOREGROUND_NOTIFICATION_ID = 1;
+    public static final String NOTIFICATION = "com.brentvatne.offline.AxDownloadService";
+    public static final String PROGRESS = "progress";
+
+    // Helper for creating a download notifications
+    private DownloadNotificationHelper notificationHelper;
+
+    public AxDownloadService() {
+        super(FOREGROUND_NOTIFICATION_ID,
+                DEFAULT_FOREGROUND_NOTIFICATION_UPDATE_INTERVAL,
+                CHANNEL_ID,
+				androidx.media3.exoplayer.R.string.exo_download_notification_channel_name,
+                0);
+    }
+
+    private void init(){
+
+        Log.d("Downloads", "+++ [AxDownloadService] init");
+
+        notificationHelper = new DownloadNotificationHelper(this, CHANNEL_ID);
+        DownloadManager downloadManager = AxOfflineManager.getInstance().getDownloadManager();
+
+        if (downloadManager != null) {
+            downloadManager.addListener(
+                    new TerminalStateNotificationHelper(
+                            this, notificationHelper, FOREGROUND_NOTIFICATION_ID + 1));
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init();
+
+    }
+
+    @NonNull
+    @Override
+    protected DownloadManager getDownloadManager() {
+        return AxOfflineManager.getInstance().getDownloadManager();
+    }
+
+    @Override
+    protected PlatformScheduler getScheduler() {
+        return Util.SDK_INT >= 21 ? new PlatformScheduler(this, JOB_ID) : null;
+    }
+
+    // Returns a notification to be displayed
+    @Override
+    protected Notification getForegroundNotification(List<Download> downloads, int i) {
+
+        Notification notification = notificationHelper.buildProgressNotification(this,
+                R.drawable.ic_download, null, null, downloads, i);
+        if (notification != null && notification.extras != null && i < downloads.size()) {
+            // Notification about download progress is sent here
+            sendNotification(notification.extras.getInt(Notification.EXTRA_PROGRESS), downloads.get(i).request.id);
+        }
+
+        return notification;
+
+    }
+
+    // A method that sends a notification
+    private void sendNotification(int progress, String content_id) {
+        Intent intent = new Intent(NOTIFICATION);
+        intent.setPackage("eus.eitb.primeran");
+        intent.putExtra(PROGRESS, progress);
+        intent.putExtra(KEY_CONTENT_ID, content_id);
+        sendBroadcast(intent);
+    }
+
+    // For listening download changes and sending notifications about it
+    private static final class TerminalStateNotificationHelper implements DownloadManager.Listener {
+
+        private final Context context;
+        private final DownloadNotificationHelper notificationHelper;
+        private int nextNotificationId;
+
+        public TerminalStateNotificationHelper(
+                Context context, DownloadNotificationHelper notificationHelper, int firstNotificationId) {
+            this.context = context.getApplicationContext();
+            this.notificationHelper = notificationHelper;
+            nextNotificationId = firstNotificationId;
+        }
+
+        @Override
+        public void onDownloadChanged(DownloadManager manager, Download download, Exception exception) {
+
+            Notification notification;
+            if (download.state == Download.STATE_COMPLETED) {
+                notification =
+                        notificationHelper.buildDownloadCompletedNotification(context,
+                                R.drawable.ic_download_done,
+                                null,
+                                Util.fromUtf8Bytes(download.request.data));
+            } else if (download.state == Download.STATE_FAILED) {
+                notification =
+                        notificationHelper.buildDownloadFailedNotification(context,
+                                R.drawable.ic_download_done,
+                                null,
+                                Util.fromUtf8Bytes(download.request.data));
+            } else {
+                return;
+            }
+            NotificationUtil.setNotification(context, nextNotificationId++, notification);
+
+        }
+    }
+
+}
---
+++ android/src/main/java/com/brentvatne/offline/AxDownloadTracker.java
@@ -0,0 +1,198 @@
+package com.brentvatne.offline;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.media3.exoplayer.DefaultRenderersFactory;
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.RenderersFactory;
+import androidx.media3.exoplayer.offline.Download;
+import androidx.media3.exoplayer.offline.DownloadCursor;
+import androidx.media3.exoplayer.offline.DownloadHelper;
+import androidx.media3.exoplayer.offline.DownloadIndex;
+import androidx.media3.exoplayer.offline.DownloadManager;
+import androidx.media3.exoplayer.offline.DownloadRequest;
+import androidx.media3.exoplayer.offline.DownloadService;
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
+import androidx.media3.exoplayer.trackselection.MappingTrackSelector;
+import androidx.media3.datasource.DataSource;
+import androidx.media3.common.util.Log;
+import androidx.media3.common.util.Util;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * A class that manages the downloads: initializes the download requests, enables to select tracks
+ * for downloading and listens for events where download status changed.
+ */
+public class AxDownloadTracker {
+
+    // Listens for changes in the tracked downloads
+    public interface Listener {
+
+        // Called when the tracked downloads changed
+        void onDownloadsChanged(int state, String id);
+    }
+
+    private static final String TAG = "Downloads";
+
+    private final Context mContext;
+    private final DataSource.Factory mDataSourceFactory;
+    private final HashMap<Uri, Download> mDownloads;
+    private final DownloadIndex mDownloadIndex;
+    private final CopyOnWriteArraySet<Listener> mListeners;
+    private DownloadHelper mDownloadHelper;
+
+    // Construction of AxDownloadTracker
+    AxDownloadTracker(Context context, DataSource.Factory dataSourceFactory, DownloadManager downloadManager) {
+        this.mContext = context.getApplicationContext();
+        mDataSourceFactory = dataSourceFactory;
+        mDownloads = new HashMap<>();
+        mListeners = new CopyOnWriteArraySet<>();
+        mDownloadIndex = downloadManager.getDownloadIndex();
+        downloadManager.addListener(new DownloadManagerListener());
+        loadDownloads();
+    }
+
+    private void loadDownloads() {
+        try {
+            DownloadCursor loadedDownloads = mDownloadIndex.getDownloads();
+            while (loadedDownloads.moveToNext()) {
+                Download download = loadedDownloads.getDownload();
+                mDownloads.put(download.request.uri, download);
+            }
+        } catch (IOException e) {
+            Log.w(TAG, "Failed to query mDownloads", e);
+        }
+    }
+
+    // Get the overrides for renderer
+    private List<DefaultTrackSelector.SelectionOverride> getRendererOverrides(int periodIndex, int rendererIndex, int[][] representations) {
+
+        List<DefaultTrackSelector.SelectionOverride> overrides = new ArrayList<>();
+        // If representations is null then all tracks are downloaded
+        if (representations != null) {
+            for (int[] indexes : representations) {
+                // For every track specification in the list check if we have the correct period and renderer
+                if (periodIndex == indexes[0] && rendererIndex == indexes[1]) {
+                    // Add a selection override for this renderer by specifying groupindex and trackindex
+                    overrides.add(new DefaultTrackSelector.SelectionOverride(indexes[2], indexes[3]));
+                }
+            }
+        }
+
+        return overrides;
+    }
+
+    /**
+     * Download only selected video tracks
+     *
+     * @param description     A description for this download, for example a video title
+     * @param representations list of representations to download in the format of
+     *                        [[periodIndex0, rendererIndex0, groupIndex0, trackIndex0], [...]]
+     *                        The structure is based on MappedTrackInfo
+     *                        For example: [[0,1,0,0]], [0,1,1,0], [0, 1, 2, 0]]
+     */
+    public void download(String description, int[][] representations) {
+
+        // Search through all periods
+        for (int periodIndex = 0; periodIndex < mDownloadHelper.getPeriodCount(); periodIndex++) {
+
+            // Get the mapped track info for this period
+            MappingTrackSelector.MappedTrackInfo mappedTrackInfo = mDownloadHelper.getMappedTrackInfo(periodIndex);
+
+            // Clear any default selections
+            mDownloadHelper.clearTrackSelections(periodIndex);
+
+            // Look through all renderers
+            for (int rendererIndex = 0; rendererIndex < mappedTrackInfo.getRendererCount(); rendererIndex++) {
+                mDownloadHelper.addTrackSelectionForSingleRenderer(
+                        periodIndex,
+                        rendererIndex,
+                        DownloadHelper.getDefaultTrackSelectorParameters(mContext),
+                        // Get the track selection overrides for this renderer
+                        getRendererOverrides(periodIndex, rendererIndex, representations));
+            }
+        }
+
+        // Create a DownloadRequest and send it to service
+        DownloadRequest downloadRequest = mDownloadHelper.getDownloadRequest(Util.getUtf8Bytes(description));
+        DownloadService.sendAddDownload(
+                mContext,
+                AxDownloadService.class,
+                downloadRequest,
+                false);
+        mDownloadHelper.release();
+    }
+
+    public void addListener(Listener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeListener(Listener listener) {
+        mListeners.remove(listener);
+    }
+
+    // Boolean for determining whether video is downloaded or not
+    public boolean isDownloaded(String url) {
+        Download download = mDownloads.get(Uri.parse(url));
+        return download != null && download.state == Download.STATE_COMPLETED;
+    }
+
+    // Find an existing download request by a URI
+    public DownloadRequest getDownloadRequest(Uri uri) {
+        Download download = mDownloads.get(uri);
+        return download != null && download.state != Download.STATE_FAILED ? download.request : null;
+    }
+
+    // Returns DownloadHelper
+    public DownloadHelper getDownloadHelper(MediaItem mediaItem, Context context) {
+        if (mDownloadHelper == null) {
+            try {
+                mDownloadHelper = getDownloadHelper(mediaItem, new DefaultRenderersFactory(context));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return mDownloadHelper;
+    }
+
+    // Clears DownloadHelper
+    public void clearDownloadHelper() {
+        if (mDownloadHelper != null) {
+            mDownloadHelper.release();
+            mDownloadHelper = null;
+        }
+    }
+
+    // Creates a DownloadHelper
+    private DownloadHelper getDownloadHelper(
+            MediaItem mediaItem, RenderersFactory renderersFactory) {
+        return DownloadHelper.forMediaItem(mContext, mediaItem, renderersFactory, mDataSourceFactory);
+    }
+
+    // For listening download changes and sending callbacks notifying about them
+    private class DownloadManagerListener implements DownloadManager.Listener {
+
+        @Override
+        public void onDownloadChanged(
+                DownloadManager downloadManager, Download download, Exception exception) {
+            mDownloads.put(download.request.uri, download);
+            for (Listener listener : mListeners) {
+                listener.onDownloadsChanged(download.state, download.request.id);
+            }
+        }
+
+        @Override
+        public void onDownloadRemoved(DownloadManager downloadManager, Download download) {
+            mDownloads.remove(download.request.uri);
+            for (Listener listener : mListeners) {
+                listener.onDownloadsChanged(download.state, download.request.id);
+            }
+        }
+    }
+}
