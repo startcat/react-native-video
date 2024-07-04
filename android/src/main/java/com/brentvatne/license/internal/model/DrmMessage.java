864b26293f346217a77671ae40ce367dc959f945
Daniel Marín <daniel.marin@start.cat>
2024-07-04 12:51:08 +0200

    License

---
+++ android/src/main/java/com/brentvatne/license/internal/model/DrmMessage.java
@@ -0,0 +1,12 @@
+package com.brentvatne.license.internal.model;
+
+/**
+ * Drm Message model
+ */
+@SuppressWarnings("unused")
+public class DrmMessage {
+    public String version;
+    public String comKeyId;
+    public boolean persistent;
+    public boolean keysBasedOnRequest;
+}
