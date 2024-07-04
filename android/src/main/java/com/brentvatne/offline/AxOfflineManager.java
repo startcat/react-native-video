63bdb8960380ec30bd2fbc246b1944574f977718
Daniel Marín <daniel.marin@start.cat>
2024-07-04 13:05:42 +0200

    offline

---
+++ android/src/main/java/com/brentvatne/offline/AxOfflineManager.java
@@ -0,0 +1,123 @@
+package com.brentvatne.offline;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.media3.database.DatabaseProvider;
+import androidx.media3.database.StandaloneDatabaseProvider;
+import androidx.media3.exoplayer.offline.DownloadManager;
+import androidx.media3.datasource.DataSource;
+import androidx.media3.datasource.DefaultDataSource;
+import androidx.media3.datasource.DefaultHttpDataSource;
+import androidx.media3.datasource.HttpDataSource;
+import androidx.media3.datasource.cache.Cache;
+import androidx.media3.datasource.cache.CacheDataSource;
+import androidx.media3.datasource.cache.NoOpCacheEvictor;
+import androidx.media3.datasource.cache.SimpleCache;
+
+import java.io.File;
+import java.util.concurrent.Executors;
+
+/**
+ * A class that manages the initialization of DownloadManager and data source factory objects.
+ */
+public class AxOfflineManager {
+
+    private static final String TAG = "Downloads";
+    private static AxOfflineManager sAxOfflineManager;
+    private DatabaseProvider databaseProvider;
+    private File mDownloadDirectory;
+    private DownloadManager mDownloadManager;
+    private AxDownloadTracker mDownloadTracker;
+    private Cache mDownloadCache;
+
+    private static String DEFAULT_DOWNLOADS_FOLDER = "";
+
+    // Return and create the AxOfflineManager instance if necessary
+    public static AxOfflineManager getInstance() {
+        if (sAxOfflineManager == null) {
+            sAxOfflineManager = new AxOfflineManager();
+        }
+        return sAxOfflineManager;
+    }
+
+    public AxDownloadTracker getDownloadTracker() {
+        return mDownloadTracker;
+    }
+
+    public DownloadManager getDownloadManager() {
+        return mDownloadManager;
+    }
+
+    public void init(Context context) {
+        init (context, new File(DEFAULT_DOWNLOADS_FOLDER));
+    }
+
+    // Initializing of AxOfflineManager
+    public synchronized void init(Context context, File tempFolder) {
+        Log.d(TAG, "init() called with: context = [" + context + "]");
+
+        DEFAULT_DOWNLOADS_FOLDER = context.getFilesDir().getAbsolutePath();
+        DEFAULT_DOWNLOADS_FOLDER = DEFAULT_DOWNLOADS_FOLDER.endsWith("/") ? DEFAULT_DOWNLOADS_FOLDER : (DEFAULT_DOWNLOADS_FOLDER + "/");
+        DEFAULT_DOWNLOADS_FOLDER += "downloads";
+
+        File folder = new File(DEFAULT_DOWNLOADS_FOLDER);
+
+        Log.d(TAG, "init() called with: older = [" + folder + "]");
+
+        if (mDownloadManager == null) {
+            mDownloadDirectory = folder;
+            mDownloadManager = new DownloadManager(
+                    context.getApplicationContext(),
+                    getDatabaseProvider(context),
+                    getDownloadCache(context),
+                    buildHttpDataSourceFactory(),
+                    Executors.newFixedThreadPool(6));
+            mDownloadTracker = new AxDownloadTracker(context, buildDataSourceFactory(context),
+                    mDownloadManager);
+        }
+    }
+
+    private File getDownloadDirectory() {
+        if (mDownloadDirectory == null) {
+            mDownloadDirectory = new File(DEFAULT_DOWNLOADS_FOLDER);
+            Log.d(TAG, "Setting value to mDownloadDirectory: " + mDownloadDirectory);
+        }
+        return mDownloadDirectory;
+    }
+
+    private synchronized Cache getDownloadCache(Context context) {
+        if (mDownloadCache == null) {
+            mDownloadCache = new SimpleCache(getDownloadDirectory(), new NoOpCacheEvictor(),
+                    getDatabaseProvider(context));
+        }
+        return mDownloadCache;
+    }
+
+    // Returns a {@link DataSource.Factory}
+    public DataSource.Factory buildDataSourceFactory(Context context) {
+        DefaultDataSource.Factory upstreamFactory =
+                new DefaultDataSource.Factory(context, buildHttpDataSourceFactory());
+        return buildReadOnlyCacheDataSource(upstreamFactory, getDownloadCache(context));
+    }
+
+    // Returns a {@link HttpDataSource.Factory}
+    private HttpDataSource.Factory buildHttpDataSourceFactory() {
+        return new DefaultHttpDataSource.Factory();
+    }
+
+    private static CacheDataSource.Factory buildReadOnlyCacheDataSource(
+            DataSource.Factory upstreamFactory, Cache cache) {
+        return new CacheDataSource.Factory()
+                .setCache(cache)
+                .setUpstreamDataSourceFactory(upstreamFactory);
+    }
+
+    private DatabaseProvider getDatabaseProvider(Context context) {
+        if (databaseProvider == null) {
+            databaseProvider = new StandaloneDatabaseProvider(context.getApplicationContext());
+        }
+        return databaseProvider;
+    }
+
+}
