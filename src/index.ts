8319b793d2436ec579c02818ef52eb487c422e1f
Daniel Marín <daniel.marin@start.cat>
2024-07-09 20:36:41 +0200

    Downloads ts lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Video from './Video';
+export { default as Downloads } from './Downloads';
 export {VideoDecoderProperties} from './specs/VideoNativeComponent';
 export * from './types';
 export type {VideoRef} from './Video';
