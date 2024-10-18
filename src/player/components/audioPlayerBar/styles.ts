0b7618035cc1662711dd38ed513316b6f58ae99d
Daniel Marín <daniel.marin@start.cat>
2024-10-18 14:05:14 +0200

    Audio Player

--- src/player/components/audioPlayerBar/styles.ts
+++ src/player/components/audioPlayerBar/styles.ts
@@ -3,21 +3,8 @@ import { SPACING } from '../../theme';
 
 export const styles = StyleSheet.create({
     container:{
-        height: 60,
-        backgroundColor:'transparent'
-    },
-    inner:{
-        flex:1
-    },
-    audioPlayer:{
-        position:'absolute',
-        bottom:-1000
-    },
-    audioPlayerTopDivider:{
-        top:0,
-        height:1,
-        width:'100%',
-        backgroundColor:'#303030'
+        backgroundColor:'transparent',
+        overflow:'hidden'
     },
     contents:{
         flex:1,
