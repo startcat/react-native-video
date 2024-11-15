3812429b2b5520e7701b95fa3ad235527b783758
Daniel Marín <daniel.marin@start.cat>
2024-11-15 19:27:14 +0100

    Time marks

---
+++ src/player/overlay/controls/buttons/timeMarkButton/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import { Text } from '@ui-kitten/components';
+import { styles } from './styles';
+import { 
+    type TimeMarkProps,
+    CONTROL_ACTION
+} from '../../../../types';
+
+const HAPTIC_OPTIONS = {
+    enableVibrateFallback: true,
+    ignoreAndroidSystemSettings: true
+};
+
+export function TimeMarkButton (props: TimeMarkProps): React.ReactElement {
+
+    const onPress = () => {
+
+        ReactNativeHapticFeedback.trigger('impactLight', HAPTIC_OPTIONS);
+
+        if (props?.onPress){
+            props?.onPress(props.id || CONTROL_ACTION.SEEK, props.value);
+
+        }
+    };
+
+    return (
+        <TouchableOpacity 
+            style={{ ...styles.container, ...styles.asButton }} 
+            onPress={onPress} 
+            accessible={true} 
+            accessibilityRole='button' 
+            accessibilityLabel={props?.accessibilityLabel}
+            pressRetentionOffset={5}
+            disabled={props.disabled}
+        >
+            <Text category='h5' style={styles.title}>{ props.title }</Text>
+        </TouchableOpacity>
+    );
+
+};
