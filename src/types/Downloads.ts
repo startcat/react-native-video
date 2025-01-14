ec0964a635cead41fc6367ed5203776440505f50
Daniel Marín <daniel.marin@start.cat>
2025-01-14 20:37:24 +0100

    Downloads MP3

--- src/types/Downloads.ts
+++ src/types/Downloads.ts
@@ -1,31 +1,31 @@
 import type {
-  Drm,
+    Drm,
 } from './video';
 
 export enum DownloadStates {
-  RESTART = 'RESTART',
-  RESTARTING = 'RESTARTING',
-  FAILED = 'FAILED',
-  REMOVING = 'REMOVING',
-  STOPPED = 'STOPPED',
-  DOWNLOADING = 'DOWNLOADING',
-  NOT_DOWNLOADED = 'NOT_DOWNLOADED',
-  COMPLETED = 'COMPLETED',
-  QUEUED = 'QUEUED'
+    RESTART = 'RESTART',
+    RESTARTING = 'RESTARTING',
+    FAILED = 'FAILED',
+    REMOVING = 'REMOVING',
+    STOPPED = 'STOPPED',
+    DOWNLOADING = 'DOWNLOADING',
+    NOT_DOWNLOADED = 'NOT_DOWNLOADED',
+    COMPLETED = 'COMPLETED',
+    QUEUED = 'QUEUED'
 };
 
 export type ConfigDownloads = {
-  disabled?: boolean;
-  user_required?: boolean;
-  download_just_wifi?: boolean;
-  log_key?: string;
+    disabled?: boolean;
+    user_required?: boolean;
+    download_just_wifi?: boolean;
+    log_key?: string;
 };
 
 export type NetworkState = {
-  isConnected: boolean;
-  isInternetReachable: boolean;
-  isWifiEnabled: boolean;
-  type: string | null;
+    isConnected: boolean;
+    isInternetReachable: boolean;
+    isWifiEnabled: boolean;
+    type: string | null;
 };
 
 export type ReadDirItem = {
@@ -39,54 +39,58 @@ export type ReadDirItem = {
 }
 
 export type DownloadItem = {
-  media?: any;
-  offlineData: {
-    session_ids:Array<string>;
-    source: {
-      id: string;
-      title: string;
-      uri: string;
-      type?: string;
-      drmScheme?: string;
-      dvrWindowMinutes?:number;
-    },
-    state: DownloadStates;
-    drm?: Drm;
-    percent?: number;
-  }
+    media?: any;
+    offlineData: {
+        session_ids:Array<string>;
+        source: {
+            id: string;
+            title: string;
+            uri: string;
+            type?: string;
+            drmScheme?: string;
+            dvrWindowMinutes?:number;
+        },
+        state: DownloadStates;
+        drm?: Drm;
+        percent?: number;
+        isBinary?: boolean;
+        fileUri?: string;
+    }
 };
 
 export type NewDownloadItem = {
-  media?: any;
-  offlineData: {
-    session_ids?:Array<string>;
-    source: {
-      id: string;
-      title: string;
-      uri: string;
-      drmScheme: string;
-      dvrWindowMinutes?:number;
-    },
-    state?: DownloadStates;
-    drm?: Drm;
-  }
+    media?: any;
+    offlineData: {
+        session_ids?:Array<string>;
+        source: {
+            id: string;
+            title: string;
+            uri: string;
+            drmScheme: string;
+            dvrWindowMinutes?:number;
+        },
+        state?: DownloadStates;
+        drm?: Drm;
+        isBinary?: boolean;
+        fileUri?: string;
+    }
 };
 
 export type SearchDownloadItem = {
-  item: DownloadItem;
-  index: number;
+    item: DownloadItem;
+    index: number;
 }
 
 // Downloads Actions Events
 
 export type OnDownloadProgressData = Readonly<{
-  id: string;
-  percent: number;
+    id: string;
+    percent: number;
 }>;
 
 export type OnDownloadStateChangedData = Readonly<{
-  id: string;
-  state: DownloadStates
+    id: string;
+    state: DownloadStates
 }>;
 
 export type OnDownloadCompletedData = Readonly<{
@@ -100,44 +104,44 @@ export type OnDownloadRemovedData = Readonly<{
 // License Events
 
 export type OnLicenseDownloadedData = Readonly<{
-  manifest: string;
+    manifest: string;
 }>;
 
 export type OnLicenseDownloadFailedData = Readonly<{
-  manifest: string;
-  error?: string;
-  code?: number;
+    manifest: string;
+    error?: string;
+    code?: number;
 }>;
 
 export type OnLicenseCheckData = Readonly<{
-  manifest: string;
-  isValid: boolean;
+    manifest: string;
+    isValid: boolean;
 }>;
 
 export type OnLicenseCheckFailedData = Readonly<{
-  manifest: string;
-  error?: string;
-  code?: number;
+    manifest: string;
+    error?: string;
+    code?: number;
 }>;
 
 export type OnLicenseReleasedData = Readonly<{
-  manifest: string;
+    manifest: string;
 }>;
 
 export type OnLicenseReleasedFailedData = Readonly<{
-  manifest: string;
-  error?: string;
-  code?: number;
+    manifest: string;
+    error?: string;
+    code?: number;
 }>;
 
 export type OnLicenseKeysRestoredData = Readonly<{
-  manifest: string;
+    manifest: string;
 }>;
 
 export type OnLicenseRestoreFailedData = Readonly<{
-  manifest: string;
-  error?: string;
-  code?: number;
+    manifest: string;
+    error?: string;
+    code?: number;
 }>;
 
 export type OnAllLicensesReleasedData = Readonly<{
@@ -145,15 +149,15 @@ export type OnAllLicensesReleasedData = Readonly<{
 }>;
 
 export type OnAllLicensesReleaseFailedData = Readonly<{
-  error?: string;
-  code?: number;
+    error?: string;
+    code?: number;
 }>;
 
 export type OnPreparedData = Readonly<{
-  mediaId: string;
+    mediaId: string;
 }>;
 
 export type OnPrepareErrorData = Readonly<{
-  mediaId: string;
-  error?: string;
+    mediaId: string;
+    error?: string;
 }>;
