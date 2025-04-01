87fa3ae4e654198979909c142182d84812781b20
Daniel Marín <daniel.marin@start.cat>
2025-04-01 14:15:10 +0200

    refactor: refactor de funciones

--- src/downloads/filesystem.ts
+++ src/downloads/filesystem.ts
@@ -38,3 +38,32 @@ export const readDirectorySize = async (dir: string): Promise<number> => {
         throw err;
     }
 };
+
+/*
+ *  Obtiene el directorio de descargas de Apple en iOS
+ * 
+ *  @param dir - Directorio base para buscar
+ *  @returns Promise<string> - Ruta del directorio de descargas de Apple o directorio base si no se encuentra
+ * 
+ */
+
+export const getAppleDownloadsDirectory = async (dir: string): Promise<string> => {
+    if (!RNFS) {
+        throw new Error('No react-native-fs module');
+    }
+    
+    try {
+        let path = '';
+        const result = await RNFS.readDir(dir);
+        
+        for (const item of result) {
+            if (item.isDirectory() && item.name?.match(/com.apple.UserManagedAssets/gi)) {
+                path = item.path;
+            }
+        }
+        
+        return path || dir;
+    } catch (err) {
+        throw err;
+    }
+};
