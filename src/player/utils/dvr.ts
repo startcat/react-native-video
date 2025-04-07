a53091bdd6bdafb437df95fbd009449a5e606593
Daniel Marín <daniel.marin@start.cat>
2025-04-07 10:41:17 +0200

    feature: hook para desplazar el tiempo del DVR cuando pausamos

--- src/player/utils/dvr.ts
+++ src/player/utils/dvr.ts
@@ -3,7 +3,8 @@
  *
  */
 
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import BackgroundTimer from 'react-native-background-timer';
 
 interface useDvrPausedSecondsProps {
     isLive: boolean;
@@ -11,9 +12,13 @@ interface useDvrPausedSecondsProps {
     paused: boolean;
 }
 
+const INTERVAL = 60000;
+
 export function useDvrPausedSeconds(props: useDvrPausedSecondsProps) {
 	const [pausedDatum, setPausedDatum] = useState<Date | null>(null);
     const [pausedSeconds, setPausedSeconds] = useState<number>(0);
+
+    const intervalObj = useRef<number>();
 
 	useEffect(() => {
 		const isPaused = !!props.paused;
@@ -25,24 +30,55 @@ export function useDvrPausedSeconds(props: useDvrPausedSecondsProps) {
             if (isPaused){
                 setPausedDatum(new Date());
 
+                intervalObj.current = BackgroundTimer.setInterval(() => {
+                    // Moveremos la barra cada 1 minuto
+                    checkDifference();
+    
+                }, INTERVAL);
+
             } else if (pausedDatum !== null){
                 
-                try {
-                    const diferenciaMilisegundos = (new Date().getTime()) - pausedDatum.getTime();
-                    const segundos = Math.floor(diferenciaMilisegundos / 1000);
-                    setPausedSeconds(segundos);
-                    setPausedDatum(null);
-
-                } catch(ex){
-                    console.warn(`[Player DVR Utils] useDvrPausedSeconds: ${ex?.message}`);
-
-                }
+                checkDifference();
+                setPausedDatum(null);
 
             }
 
 		}
 
+        return () => {
+            clearInterval();
+
+        };
+
 	}, [props.paused, props.isLive, props.isDVR]);
+
+    const clearInterval = () => {
+
+        if (intervalObj.current){
+            BackgroundTimer.clearInterval(intervalObj.current);
+        }
+
+    }
+
+    const checkDifference = () => {
+
+        if (pausedDatum !== null){
+                
+            try {
+                const diferenciaMilisegundos = (new Date().getTime()) - pausedDatum.getTime();
+                const segundos = Math.floor(diferenciaMilisegundos / 1000);
+                setPausedSeconds(segundos);
+                setPausedDatum(new Date());
+                console.warn(`[Player DVR Utils] useDvrPausedSeconds ${segundos} seg.`);
+
+            } catch(ex){
+                console.warn(`[Player DVR Utils] useDvrPausedSeconds: ${ex?.message}`);
+
+            }
+
+        }
+
+    }
 
 	// Usamos useMemo para devolver un objeto estable que no cause re-renders innecesarios
 	return pausedSeconds;
