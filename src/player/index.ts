61cc94fac554ba8ea7651066ea7031f2bcff2025
Daniel Marín <daniel.marin@start.cat>
2025-07-17 15:26:36 +0200

    refactor: gestor del progreso en core/progress

--- src/player/index.ts
+++ src/player/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './components/overlay';
-export * from './utils';
+export * from './features';
 export * from './flavours';
 export * from './types';
+export * from './utils';
