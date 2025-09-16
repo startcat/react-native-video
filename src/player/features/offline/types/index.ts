c01ee9b731d1fb92beafef6969fd87a901761ff5
Daniel Marín <daniel.marin@start.cat>
2025-09-16 17:21:57 +0200

    feat: refactor sistema de descargas y reproducción offline

--- src/player/features/offline/types/index.ts
+++ src/player/features/offline/types/index.ts
@@ -3,6 +3,7 @@ export * from './drm';
 export * from './events';
 export * from './items';
 export * from './network';
+export * from './persistence';
 export * from './profiles';
 export * from './queue';
 export * from './storage';
